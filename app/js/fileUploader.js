81cc82aeb8d551abb65781e804d9ca061b15c0b9
Timofey Ponomarev <timlefff@gmail.com>
2015-09-03 20:48:55 +0700

    Инициализация загрузчика

--- app/js/fileUploader.js
+++ app/js/fileUploader.js
@@ -6,16 +6,21 @@ var wm_fileuploader;
 
 		wm_fileuploader = (function() {
 
+			//URL до нашего php файла обработчкиа
 			var url = 'php/upload.php';
 
 			$('.send_files').fileupload({
 				url      : url,
 				dataType : 'json',
 				done: function (e, data) {
+					//В data.result у нас вся инва по файлу
+
 					console.log( data.result );
 					//$('#fsdf').html('<img src="' + data.result.file_url + '" />');
 				},
 				progressall: function (e, data) {
+					//Прогресс бар на будущее
+
 					/*var progress = parseInt(data.loaded / data.total * 100, 10);
 					$('#progress').text(
 						progress + '%'
--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,7 @@
 		 * Инициализируем наши модули
 		 */
 		//$('.push-top').length && as_toTop.init();
+		wm_fileuploader.init();
 
 	});
 })( jQuery );
